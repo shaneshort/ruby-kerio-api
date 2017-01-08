d63d274303291e6f24140060baa86f4589867631
Petr Baloun <user@bender.lan>
2017-01-08 17:32:53 +0100

    Fixed exception handling in method chaining

--- lib/kerio-api/chainable_method.rb
+++ lib/kerio-api/chainable_method.rb
@@ -2,19 +2,18 @@ module Kerio
 	module Api
 		module ChainableMethod
 			def next_method(params)
-
 				# prefer special implementation over the generic one
 				begin
 					source = "kerio-api/method/#{params[:names].join('/')}.rb".downcase
 					require source
 
 					method_class = params[:names].inject(Kerio::Api::Method) {|o,c| o.const_get c.capitalize}
-					method_instance = method_class.new(params)
 
-				rescue NameError, LoadError
-					method_instance = Kerio::Api::Method::Generic.new(params)
+				rescue NameError, LoadError => e
+					method_class = Kerio::Api::Method::Generic
 				end
 
+				method_instance = method_class.new(params)
 				return method_instance
 			end
 		end
